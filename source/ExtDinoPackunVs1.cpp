c8f2b0678c8eb7be136141c1280ff0927d446270
GlacialVitality <glacialvitality@gmail.com>
2026-05-24 16:22:25 -0400

    get the thing working frfr quh

--- source/ExtDinoPackunVs1.cpp
+++ source/ExtDinoPackunVs1.cpp
@@ -5,23 +5,20 @@
 //exeAngryDemo__14DinoPackunDemoFv=0x800B20F0
 //exeDownDemo__14DinoPackunDemoFv=0x800B2310
 
-void openingDemoEnd(DinoPackun* pDino) {
-    if (MR::isBckStopped(pDino))
-        MR::startStageBGM("MBGM_GALAXY_INTER", false);
+void openingDemo() {
+    MR::startStageBGM("MBGM_GALAXY_INTER", false);
 }
 
-kmCall(0x800B1D20, openingDemoEnd);
+kmCall(0x800B1EF8, openingDemo);
 
-void cryDemoEnd(DinoPackun* pDino) {
-    if (MR::isBckStopped(pDino))
-        MR::startStageBGM("MBGM_SMG2_BOSS_05A", false);
+void cryDemoEnd() {
+    MR::startStageBGM("MBGM_SMG2_BOSS_07A", false);
 }
 
-kmCall(0x800B1FA0, cryDemoEnd);
+kmCall(0x800B2070, cryDemoEnd);
 
-void angryDemoEnd(DinoPackun* pDino) {
-    if (MR::isBckStopped(pDino))
-        MR::startStageBGM("MBGM_SMG2_BOSS_05B", false);
+void angryDemoEnd() {
+    MR::startStageBGM("MBGM_SMG2_BOSS_07B", false);
 }
 
-kmCall(0x800B20F0, angryDemoEnd);
+kmCall(0x800B2294, angryDemoEnd);
